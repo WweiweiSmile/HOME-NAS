df820a49215b178ae71a409f0cc558a53f552a2a
WweiweiSmile <1119062049@qq.com>
2022-08-24 22:25:41 +0800

    设置路由别名

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import VideoPlay from "./pages/VideoPlay"
+import VideoPlay from "@/pages/VideoPlay"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
