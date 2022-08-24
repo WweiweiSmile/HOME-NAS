debc880abd5e72a12acb84325a5db3491eb1ba9b
WweiweiSmile <1119062049@qq.com>
2022-08-24 22:12:42 +0800

    路由添加

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import VideoPlay from "./pages/VideoPlay"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<App />}></Route>
+        <Route path={'/video-play'} element={<VideoPlay></VideoPlay>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/pages/VideoPlay/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+const VideoPlay = () => {
+
+    return <div>{"这是视频播放页面"}</div>
+}
+
+export default VideoPlay
